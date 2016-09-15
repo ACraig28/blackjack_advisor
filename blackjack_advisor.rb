4b09b43f7c076c6ebf687f0c907e9a359c917651
Alexandra Craig <alexandra.craig@gmail.com>
2016-09-15 15:13:14 -0400

    Created two methods and loop to find value of first card

---
+++ blackjack_advisor.rb
@@ -0,0 +1,37 @@
+def face_check(card)
+  card == "J" || card == "Q" || card == "K"
+end
+
+def ace_check(card)
+  card == "A"
+end
+
+puts "Welcome to the Blackjack Advisor"
+puts "Before we get started, know that you can enter facecards as 'J', 'Q', 'K' or the value '10'. An ace, however, must be entered as 'A'."
+puts "Let's get started!"
+
+loop do
+  puts "Please enter your first card:"
+  first_card = gets.chomp
+  if face_check(first_card)
+    first_value = 10
+    puts first_value
+    break
+  elsif ace_check(first_card)
+    first_value = 11
+    puts first_value
+    break
+  elsif first_card.to_i > 10
+    puts "That is not an acceptable value. Please enter a number 1-10, 'J','Q','K' or 'A'."
+  else
+    first_value = first_card
+    puts first_value
+    break
+  end
+end
+
+
+# puts "Please enter your second card"
+# second_card = gets.chomp
+# puts "Please enter the dealer's card"
+# dealers_card = gets.chomp
